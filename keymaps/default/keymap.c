43fa07e6bad127f9d724795beeb16276fded5f97
dreipunkteinsvier <dreipunkteinsvier@gmail.com>
2023-05-14 14:23:17 +0200

    added files for the waveshare rp2040 variant

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -9,13 +9,13 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_ESC,                                                                                             KC_BSPC,
+    RGB_TOG,                                                                                            RGB_MOD,
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,
     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
-    KC_LSFT,  KC_LALT,            KC_TAB,   KC_ENT,             KC_SPC,   KC_DEL,             KC_RALT,  KC_RSFT,
+    KC_LSFT,  KC_LALT,            KC_TAB,   KC_ENT,             KC_SPC,   KC_DEL,             KC_RALT,  RGB_MOD,
                                 //click ,   right  , down   ,   left  ,   up
-                                  KC_MUTE,  KC_VOLU, KC_PGDN,   KC_VOLD,  KC_PGUP
+                                  KC_MUTE,  KC_VOLU, RGB_VAD,   KC_VOLD,  RGB_VAI
   ),
   
   [_LOWER] = LAYOUT(
---
+++ zero/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 dreipunkteinsvier (@dreipunkteinsvier)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_LED_COUNT RGBLED_NUM
+    #define WS2812_DI_PIN GP2
+#endif
